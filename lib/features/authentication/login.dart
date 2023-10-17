cc11e4570076e5301aeb5e28462217d33f0c137e
dorafina <thessyzilla@gmail.com>
2023-10-17 11:33:58 +0100

    app icon

--- lib/features/authentication/login.dart
+++ lib/features/authentication/login.dart
@@ -10,124 +10,122 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const CircleAvatar(
-                radius: 100,
-                backgroundImage: AssetImage('assets/power2.jpg'),
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const CircleAvatar(
+              radius: 100,
+              backgroundImage: AssetImage('assets/power2.jpg'),
+            ),
+            const SizedBox(height: 16.0),
+            const Center(),
+            const SizedBox(height: 16.0),
+            Center(
+              child: Text(
+                'Your everyday consumption record',
+                style: GoogleFonts.baiJamjuree(fontSize: 20),
               ),
-              const SizedBox(height: 16.0),
-              const Center(),
-              const SizedBox(height: 16.0),
-              Center(
-                child: Text(
-                  'Your everyday consumption record',
-                  style: GoogleFonts.baiJamjuree(fontSize: 20),
-                ),
-              ),
-              const SizedBox(height: 16),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  SizedBox(
-                    width: 300,
-                    height: 50,
-                    child: ElevatedButton(
-                      onPressed: () async {
-                        GoRouter.of(context).push(RouteNames.register);
-                      },
-                      style: OutlinedButton.styleFrom(
-                        shape: const RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(
-                              50,
-                            ),
+            ),
+            const SizedBox(height: 16),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                SizedBox(
+                  width: 300,
+                  height: 50,
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      GoRouter.of(context).push(RouteNames.register);
+                    },
+                    style: OutlinedButton.styleFrom(
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(
+                            50,
                           ),
                         ),
-                        maximumSize: const Size(double.infinity, 100),
-                        backgroundColor: const Color(0xFF035515),
-                        side: const BorderSide(
-                          color: Color(0xFF035515),
-                        ),
                       ),
-                      child: const Text(
-                        'Sign up',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold,
-                        ),
+                      maximumSize: const Size(double.infinity, 100),
+                      backgroundColor: const Color(0xFF035515),
+                      side: const BorderSide(
+                        color: Color(0xFF035515),
+                      ),
+                    ),
+                    child: const Text(
+                      'Sign up',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
                   ),
-                ],
-              ),
-              const SizedBox(height: 16),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    width: 300,
-                    height: 50,
-                    child: ElevatedButton(
-                      onPressed: () async {
-                        GoRouter.of(context).push(RouteNames.signin);
-                      },
-                      style: OutlinedButton.styleFrom(
-                        shape: const RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(
-                              50,
-                            ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 16),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 300,
+                  height: 50,
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      GoRouter.of(context).push(RouteNames.signin);
+                    },
+                    style: OutlinedButton.styleFrom(
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(
+                            50,
                           ),
                         ),
-                        maximumSize: const Size(double.infinity, 100),
-                        backgroundColor: Colors.white,
-                        side: const BorderSide(
-                          color: Colors.white,
-                        ),
                       ),
-                      child: const Text(
-                        'Login',
-                        style: TextStyle(
-                          color: Color(0xFF035515),
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold,
-                        ),
+                      maximumSize: const Size(double.infinity, 100),
+                      backgroundColor: Colors.white,
+                      side: const BorderSide(
+                        color: Colors.white,
+                      ),
+                    ),
+                    child: const Text(
+                      'Login',
+                      style: TextStyle(
+                        color: Color(0xFF035515),
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
                   ),
-                ],
-              ),
-              const SizedBox(height: 20),
-              Container(
-                margin: const EdgeInsets.symmetric(horizontal: 16),
-                child: InkWell(
-                  onTap: () {
-                    GoRouter.of(context).push(RouteNames.login);
-                  },
-                  child: RichText(
-                    text: const TextSpan(
-                        text: 'New Around here? ',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontFamily: 'DM Sans',
-                          fontSize: 16,
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 16),
+              child: InkWell(
+                onTap: () {
+                  GoRouter.of(context).push(RouteNames.signin);
+                },
+                child: RichText(
+                  text: const TextSpan(
+                      text: 'New Around here? ',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontFamily: 'DM Sans',
+                        fontSize: 16,
+                      ),
+                      children: [
+                        TextSpan(
+                          text: 'Sign in',
+                          style: TextStyle(
+                              color: Color(0xFF035515), fontSize: 16),
                         ),
-                        children: [
-                          TextSpan(
-                            text: 'Sign in',
-                            style: TextStyle(
-                                color: Color(0xFF035515), fontSize: 16),
-                          ),
-                        ]),
-                  ),
+                      ]),
                 ),
               ),
-            ]),
-      ),
+            ),
+          ]),
     );
   }
 }
--- lib/features/authentication/register.dart
+++ lib/features/authentication/register.dart
@@ -22,183 +22,188 @@ class RegisterState extends State<Register> {
   final TextEditingController _passwordController = TextEditingController();
 
   final TextEditingController _confirmPasswordController =
-      TextEditingController();
+  TextEditingController();
 
   final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SingleChildScrollView(
-            child: Form(
-      key: _formKey,
-      child: SingleChildScrollView(
-        padding: const EdgeInsets.only(top: 80),
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            Center(
-              child: Container(
-                margin: const EdgeInsets.only(bottom: 25),
-                child: const Text(
-                  "Register and lets get started ",
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold),
-                ),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-              child: TextFormField(
-                controller: _firstAndNameController,
-                decoration: const InputDecoration(
-                  labelText: "First and last Name",
-                  border: OutlineInputBorder(),
-                  alignLabelWithHint: false,
-                  filled: true,
-                ),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-              child: TextFormField(
-                controller: _emailController,
-                decoration: const InputDecoration(
-                  labelText: "Email Address",
-                  border: OutlineInputBorder(),
-                  alignLabelWithHint: false,
-                  filled: true,
-                ),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-              child: TextFormField(
-                controller: _phoneNumberController,
-                decoration: const InputDecoration(
-                  labelText: "start with your country code (+234)",
-                  border: OutlineInputBorder(),
-                  alignLabelWithHint: false,
-                  filled: true,
-                ),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-              child: TextFormField(
-                obscureText: passwordVisible,
-                controller: _passwordController,
-                decoration: InputDecoration(
-                  labelText: "Password",
-                  // labelText: "Password",
-                  helperText: "Password must contain special Characters",
-                  hintStyle: const TextStyle(color: Colors.black54),
-                  border: const OutlineInputBorder(),
-                  suffixIcon: IconButton(
-                    color: Colors.teal.shade200,
-                    icon: Icon(passwordVisible
-                        ? Icons.visibility
-                        : Icons.visibility_off),
-                    onPressed: () {
-                      setState(
-                        () {
-                          passwordVisible = !passwordVisible;
-                        },
-                      );
-                    },
+      body: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.only(top: 80),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center, children: [
+                Center(
+                  child: Container(
+                    margin: const EdgeInsets.only(bottom: 25),
+                    child: const Text(
+                      "Register and lets get started ",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
+                    ),
                   ),
-                  alignLabelWithHint: false,
-                  filled: true,
                 ),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-              child: TextFormField(
-                obscureText: passwordVisible,
-                controller: _confirmPasswordController,
-                decoration: InputDecoration(
-                  labelText: "Confirm Password",
-                  helperText: "Password must contain special Characters",
-                  // labelText: "Password",
-                  border: const OutlineInputBorder(),
-                  suffixIcon: IconButton(
-                    color: Colors.teal.shade200,
-                    icon: Icon(passwordVisible
-                        ? Icons.visibility
-                        : Icons.visibility_off),
-                    onPressed: () {
-                      setState(
-                        () {
-                          passwordVisible = !passwordVisible;
-                        },
-                      );
-                    },
+                Container(
+                  padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                  child: TextFormField(
+                    controller: _firstAndNameController,
+                    decoration: const InputDecoration(
+                      labelText: "First and last Name",
+                      border: OutlineInputBorder(),
+                      alignLabelWithHint: false,
+                      filled: true,
+                    ),
                   ),
-                  alignLabelWithHint: false,
-                  filled: true,
                 ),
-              ),
-            ),
-            RichText(
-              text: TextSpan(
-                text: 'I accept terms and conditions for this app. ',
-                style: DefaultTextStyle.of(context).style,
-                children: const [
-                  WidgetSpan(
-                    child: Padding(
-                      padding: EdgeInsets.only(right: 4.0),
-                      child: Icon(
-                        Icons.check_box,
-                        color: Colors.black,
+                Container(
+                  padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                  child: TextFormField(
+                    controller: _emailController,
+                    decoration: const InputDecoration(
+                      labelText: "Email Address",
+                      border: OutlineInputBorder(),
+                      alignLabelWithHint: false,
+                      filled: true,
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                  child: TextFormField(
+                    controller: _phoneNumberController,
+                    decoration: const InputDecoration(
+                      labelText: "start with your country code (+234)",
+                      border: OutlineInputBorder(),
+                      alignLabelWithHint: false,
+                      filled: true,
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                  child: TextFormField(
+                    obscureText: passwordVisible,
+                    controller: _passwordController,
+                    decoration: InputDecoration(
+                      labelText: "Password",
+                      // labelText: "Password",
+                      helperText: "Password must contain special Characters",
+                      hintStyle: const TextStyle(color: Colors.black54),
+                      border: const OutlineInputBorder(),
+                      suffixIcon: IconButton(
+                        color: Colors.teal.shade200,
+                        icon: Icon(passwordVisible
+                            ? Icons.visibility
+                            : Icons.visibility_off),
+                        onPressed: () {
+                          setState(
+                                () {
+                              passwordVisible = !passwordVisible;
+                            },
+                          );
+                        },
+                      ),
+                      alignLabelWithHint: false,
+                      filled: true,
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                  child: TextFormField(
+                    obscureText: passwordVisible,
+                    controller: _confirmPasswordController,
+                    decoration: InputDecoration(
+                      labelText: "Confirm Password",
+                      helperText: "Password must contain special Characters",
+                      // labelText: "Password",
+                      border: const OutlineInputBorder(),
+                      suffixIcon: IconButton(
+                        color: Colors.teal.shade200,
+                        icon: Icon(passwordVisible
+                            ? Icons.visibility
+                            : Icons.visibility_off),
+                        onPressed: () {
+                          setState(
+                                () {
+                              passwordVisible = !passwordVisible;
+                            },
+                          );
+                        },
+                      ),
+                      alignLabelWithHint: false,
+                      filled: true,
+                    ),
+                  ),
+                ),
+                RichText(
+                  text: TextSpan(
+                      style: DefaultTextStyle
+                          .of(context)
+                          .style,
+                      children: const [
+                        WidgetSpan(
+                          child: Padding(
+                            padding: EdgeInsets.only(right: 4.0),
+                            child: Icon(
+                              Icons.check_box,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                        TextSpan(
+                          text: 'I accept terms and conditions.',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.blue,
+                          ),
+                        ),
+                      ]
+                  ),
+                ),
+
+                Container(
+                  width: 300,
+                  height: 50,
+                  margin: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      GoRouter.of(context).push(RouteNames.signin);
+                    },
+                    style: ElevatedButton.styleFrom(
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(50),
+                        ),
+                      ),
+                      maximumSize: const Size(double.infinity, 100),
+                      backgroundColor: const Color(0xFF035515),
+                      side: const BorderSide(
+                        color: Colors.teal,
+                      ),
+                    ),
+                    child: const Text(
+                      'Register',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
                   ),
-                  TextSpan(
-                    text: 'Terms and Conditions',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.blue,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              width: 300,
-              height: 50,
-              margin: const EdgeInsets.all(8.0),
-              child: ElevatedButton(
-                onPressed: () {
-                  GoRouter.of(context).push(RouteNames.signin);
-                },
-                style: ElevatedButton.styleFrom(
-                  shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(50),
-                    ),
-                  ),
-                  maximumSize: const Size(double.infinity, 100),
-                  backgroundColor: const Color(0xFF035515),
-                  side: const BorderSide(
-                    color: Colors.teal,
-                  ),
                 ),
-                child: const Text(
-                  'Register',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
+              ]),
             ),
-          ]),
+          ),
         ),
       ),
-    )));
+    );
   }
 }
