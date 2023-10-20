4c0bb97efcbc03e7bbcf6f1e5e228abee2f8316e
danroyal001 <danroyaleffiong@gmail.com>
2023-10-20 11:51:34 +0100

    _

--- lib/features/home/profile_screen.dart
+++ lib/features/home/profile_screen.dart
@@ -22,80 +22,78 @@ class _ProfileScreenState extends State<ProfileScreen> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: SafeArea(
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                GlobalAppBar(),
-                Obx(
-                  () => CircleAvatar(
-                    radius: 50.0,
-                    // backgroundImage: NetworkImage(
-                    //     _authController.currentUser.value?.photoURL ??
-                    //         ''),
-                  ),
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              GlobalAppBar(),
+              Obx(
+                () => CircleAvatar(
+                  radius: 50.0,
+                  // backgroundImage: NetworkImage(
+                  //     _authController.currentUser.value?.photoURL ??
+                  //         ''),
                 ),
-                Container(
-                  width: double.infinity,
-                  height: 40,
-                  margin: const EdgeInsets.only(
-                    right: 84,
-                    left: 84,
-                    top: 10,
-                  ),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      GoRouter.of(context).push(RouteNames.editprofilescreen);
-                    },
-                    style: ElevatedButton.styleFrom(
-                      shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(
-                            10,
-                          ),
+              ),
+              Container(
+                width: double.infinity,
+                height: 40,
+                margin: const EdgeInsets.only(
+                  right: 84,
+                  left: 84,
+                  top: 10,
+                ),
+                child: ElevatedButton(
+                  onPressed: () {
+                    GoRouter.of(context).push(RouteNames.editprofilescreen);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(
+                          10,
                         ),
                       ),
-                      maximumSize: const Size(double.infinity, 100),
-                      backgroundColor: const Color(0XFF1C8E77),
                     ),
-                    child: const Text(
-                      'Edit Profile',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontFamily: 'Satoshi',
-                          fontSize: 14),
+                    maximumSize: const Size(double.infinity, 100),
+                    backgroundColor: const Color(0XFF1C8E77),
+                  ),
+                  child: const Text(
+                    'Edit Profile',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'Satoshi',
+                        fontSize: 14),
+                  ),
+                ),
+              ),
+              Container(height: 30),
+              const Divider(
+                color: Color(0xFF000000),
+              ),
+              Container(height: 15),
+              TextButton(
+                child: ListTile(
+                  contentPadding:
+                      const EdgeInsets.symmetric(vertical: 4, horizontal: 20),
+                  leading: Container(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.asset(
+                      'svgs/Logout.png',
+                      height: 20,
+                      width: 20,
+                    ),
+                  ),
+                  title: const Text(
+                    'Logout',
+                    style: TextStyle(
+                      color: Colors.black,
                     ),
                   ),
                 ),
-                Container(height: 30),
-                const Divider(
-                  color: Color(0xFF000000),
-                ),
-                Container(height: 15),
-                TextButton(
-                  child: ListTile(
-                    contentPadding:
-                        const EdgeInsets.symmetric(vertical: 4, horizontal: 20),
-                    leading: Container(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Image.asset(
-                        'svgs/Logout.png',
-                        height: 20,
-                        width: 20,
-                      ),
-                    ),
-                    title: const Text(
-                      'Logout',
-                      style: TextStyle(
-                        color: Colors.black,
-                      ),
-                    ),
-                  ),
-                  onPressed: () async {},
-                )
-              ],
-            ),
+                onPressed: () async {},
+              )
+            ],
           ),
         ),
         bottomNavigationBar: GlobalBottomNav(),
